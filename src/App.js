9da5f64c028a2ada81a11780563a751a33136019
Ana <anaconradof@gmail.com>
2024-02-07 13:35:35 -0300

    feat/pagina de costumização

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./Pages/Login/Login.js";
 import Cadastro from "./Pages/Cadastro/Cadastro.js";
+import Costumizacao from "./Pages/Costumizacao/Costumizacao.js";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/costumizacao" element={<Costumizacao />} />
     </Routes>
     </BrowserRouter>
   );
